cee8512cfbbb618385817fefd18c34464bd024ba
joamarver3 <joanmarquesverdegal1@gmail.com>
2024-10-21 09:15:57 +0200

    SUbiendo ejercicios hechos hasta ahora. Commit inicial

--- src/Tema2/ejer3.java
+++ src/Tema2/ejer3.java
@@ -1,7 +1,14 @@
 package Tema2;
 
+import java.util.Scanner;
+
 public class ejer3 {
     public static void main(String[] args) {
-
+        Scanner user = new Scanner(System.in);
+        System.out.println("dime la base del triangulo");
+        double base = user.nextDouble();
+        System.out.println("dime la altura del triangulo");
+        double altura = user.nextDouble();
+        System.out.println("el area es " + base * altura / 2);
     }
 }
