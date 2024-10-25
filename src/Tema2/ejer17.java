9b7f500f670681ebc441732ec650dc518cc38e98
joamarver3 <joanmarquesverdegal1@gmail.com>
2024-10-25 10:34:06 +0200

    SUbiendo ejercicios hechos hasta ahora. Commit inicial

---
+++ src/Tema2/ejer17.java
@@ -0,0 +1,37 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class ejer17 {
+    public static void main(String[] args) {
+        double areacua,arearec,percua,perrec,areatri,ladocua,lado1,altrec, basetri, alttri;
+
+        int num1;
+        Scanner numuser =new Scanner(System.in);
+        System.out.println("pulsa el numero 0 si quieres que muestre el area y perimetro de un cuadrado, el 1 para area y perimtreo de un rectangulo y 2 para el area de un triangulo");
+        num1 = numuser.nextInt();
+        if (num1 == 0) {
+            System.out.println("ahora dime el lado del cuadrado");
+            ladocua = numuser.nextDouble();
+            areacua = ladocua * ladocua;
+            percua = ladocua * 4;
+            System.out.println(" el resultado del area es " + areacua + " y el perimetro es " + percua);
+        }
+        if (num1 == 1) {
+            System.out.println("dime la base del rectangulo y la altura");
+            lado1 = numuser.nextDouble();
+            altrec =numuser.nextDouble();
+            arearec = lado1 * altrec;
+            perrec= altrec * 2 + lado1 * 2;
+            System.out.println("el area es " + arearec + " y el perimetro es "+ perrec);
+        }
+        if (num1 == 2) {
+            System.out.println("dime la base y la altura del triangulo");
+            basetri = numuser.nextDouble();
+            alttri = numuser.nextDouble();
+            areatri = basetri * alttri / 2;
+            System.out.println("el area es " + areatri);
+        }
+
+    }
+}
---
+++ src/Tema2/ejer18.java
@@ -0,0 +1,19 @@
+package Tema2;
+
+import java.util.Scanner;
+
+public class ejer18 {
+    public static void main(String[] args) {
+        double raiz,num1;
+        Scanner numuser = new Scanner(System.in);
+        System.out.println("dime un numero");
+        num1 = numuser.nextDouble();
+        raiz = Math.sqrt(num1);
+        if (num1 < 0) {
+            System.out.println("numero invalido");
+        } else {
+            System.out.println(" la raiz es " + raiz);
+        }
+
+    }
+}
