523a9b78afa99ba719a48e040cb9796274ee2f31
joamarver3 <joanmarquesverdegal1@gmail.com>
2025-01-09 13:43:55 +0100

    SUbiendo ejercicios hechos hasta ahora. Commit inicial

---
+++ src/Arrays/JuegoMosca.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class JuegoMosca {
+    public static void main(String[] args) {
+        Scanner userprint = new Scanner(System.in);
+        int[] array= new int[15];
+
+    }
+    public static void PosAleatoria(int[]array){
+
+    }
+}
