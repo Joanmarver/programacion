03ead1d348de749dd2c761e5a467adeae2b7eda2
joamarver3 <joanmarquesverdegal1@gmail.com>
2025-01-10 13:49:01 +0100

    SUbiendo ejercicios hechos hasta ahora. Commit inicial

---
+++ src/Arrays/hoteles.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class hoteles {
+    public static void main(String[] args) {
+        int[] habitaciones = new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        mostrararray(habitaciones);
+
+    }
+    public static int[] cobertura(int[] habitaciones){
+
+        Scanner userprint = new Scanner(System.in);
+        System.out.println("Selecciona el  numero de habitacion");
+        int respuesta = userprint.nextInt();
+        System.out.println("selecciona la potencia del router");
+        int respuestados = userprint.nextInt();
+        habitaciones[respuesta]=respuestados;
+        int asce = 1;
+        for (int i = 0; i < respuestados; i++){
+
+            if ( habitaciones[respuesta + asce] < 0 )
+            habitaciones[respuesta + asce]= respuestados - asce;
+            habitaciones[respuesta - asce]= respuestados - asce;
+
+            asce++;
+        }
+        return habitaciones;
+    }
+    public static void mostrararray(int [] habitaciones){
+        cobertura(habitaciones);
+        for (int i = 0; i< habitaciones.length; i++ )
+            System.out.print(habitaciones[i ] + ",");
+
+    }
+}
