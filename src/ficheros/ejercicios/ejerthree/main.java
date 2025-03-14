4df90bace6e373f604f146ac48a267985933565b
joamarver3 <joanmarquesverdegal1@gmail.com>
2025-03-14 12:44:16 +0100

    werw

--- src/ficheros/ejercicios/ejerthree/main.java
+++ src/ficheros/ejercicios/ejerthree/main.java
@@ -6,31 +6,32 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.sql.ClientInfoStatus;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
-public class mian {
+public class main {
     public static void main(String[] args) {
         File nuevoarchivo = new File("archivo_B.txt");
         Path Patharchivo = Paths.get("archivo_B.txt");
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("dime el archivo que quieres leer");
-        String archivo = scanner.next();
+        String archivo = "resources/Documentos/" + scanner.nextLine();
         String content=null;
         try {
 
                 List<String> lines= Files.readAllLines(Paths.get(archivo));
                 Collections.sort(lines);
-            for (String linea: lines){
-                Files.writeString(Patharchivo,linea, StandardOpenOption.APPEND);
+
+                if (!Files.exists(Patharchivo)){
+                    Files.createFile(Patharchivo);
+                }
+            Files.write(Patharchivo,lines, StandardOpenOption.APPEND);
 
 
-            }
             String contenido = Files.readString(Patharchivo);
+            System.out.println(contenido);
 
         } catch (IOException e) {
             e.printStackTrace();
