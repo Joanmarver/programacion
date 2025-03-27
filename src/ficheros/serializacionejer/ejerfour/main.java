215d2635e762cccb432399af15e40769ffb8c1bb
joamarver3 <joanmarquesverdegal1@gmail.com>
2025-03-27 13:00:08 +0100

    werw

---
+++ src/ficheros/serializacionejer/ejerfour/main.java
@@ -0,0 +1,14 @@
+package ficheros.serializacionejer.ejerfour;
+
+import java.io.FileOutputStream;
+
+public class main {
+    public static void main(String[] args) {
+        Persona persona= new Persona("joan",19);
+        try{
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
