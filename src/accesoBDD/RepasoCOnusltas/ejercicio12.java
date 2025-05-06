afc138136ee795a4aebba4545f788fb0b869d850
joamarver3 <joanmarquesverdegal1@gmail.com>
2025-05-06 10:48:33 +0200

    werw

--- src/accesoBDD/RepasoCOnusltas/ejercicio12.java
+++ src/accesoBDD/RepasoCOnusltas/ejercicio12.java
@@ -6,18 +6,29 @@ public class ejercicio12 {
     public static void main(String[] args) {
         String nombre="Nymphadora";
         String apellido ="Tonks";
-        String id_casa= "4";
-        String anyo_curso="7";
-        String fecha="'1973-11-25'";
+        int id_casa= 4;
+        int anyo_curso=7;
+        Date fecha= Date.valueOf("1973-12-25");
+
         String insert= "INSERT INTO Estudiante (nombre,apellido,id_casa,anyo_curso, fecha_nacimiento) VALUES (?, ?, ?, ?, ?)";
+        int idGenerado = -1;
         try(Connection bdd = DriverManager.getConnection("jdbc:postgresql://ad-postgres.cljzaoejhj87.us-east-1.rds.amazonaws.com:5432/hogwarts","postgres","12345678")){
             PreparedStatement p= bdd.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
            p.setString(1,nombre);
            p.setString(2,apellido);
-           p.setString(3,id_casa);
-           p.setString(4,anyo_curso);
-           p.setString(5,fecha);
+           p.setInt(3,id_casa);
+           p.setInt(4,anyo_curso);
+           p.setDate(5,fecha);
 
+           int filasAfectadas = p.executeUpdate();
+            System.out.println("filas afectadas  al insertar " + filasAfectadas);
+            ResultSet rs = p.getGeneratedKeys();
+            if (rs.next()){
+              idGenerado = rs.getInt(1);
+            }
+            rs.close();
+            p.close();
+            System.out.println("Estudiante generado con ID: " + idGenerado);
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
