1e03a39c3b4760be38c74095e6619eb45f073674
joamarver3 <joanmarquesverdegal1@gmail.com>
2025-05-09 11:56:18 +0200

    werw

--- src/accesoBDD/Ejercicios/ejercicio1.java
+++ src/accesoBDD/Ejercicios/ejercicio1.java
@@ -5,39 +5,49 @@ import java.util.Scanner;
 
 public class ejercicio1 {
     public static void main(String[] args) {
+        boolean inicio = true;
         Scanner scanner= new Scanner(System.in);
-        System.out.println(" 1.Consulta de Estudiantes por casa" +
-                "\n 2.Obtener la mascota de un estudiante especifico" +
-                "\n 3.Numero de estudiantes por casa" +
-                "\n 4.Insertar una nueva asignatura" +
-                "\n 5.Modificar el aula de una asignatura" +
-                "\n 6.Eliminar una Asignatura");
-        int respuesta= scanner.nextInt();
-        switch (respuesta){
-            case 1:{
-                estudiante_casa(scanner);
-                break;
+        while (inicio) {
+            System.out.println(" 1.Consulta de Estudiantes por casa" +
+                    "\n 2.Obtener la mascota de un estudiante especifico" +
+                    "\n 3.Numero de estudiantes por casa" +
+                    "\n 4.Insertar una nueva asignatura" +
+                    "\n 5.Modificar el aula de una asignatura" +
+                    "\n 6.Eliminar una Asignatura" +
+                    "\n 7.Salir");
+            int respuesta = scanner.nextInt();
+            switch (respuesta) {
+                case 1: {
+                    estudiante_casa(scanner);
+                    break;
+                }
+                case 2: {
+                    mascota_estudiante(scanner);
+                    break;
+                }
+                case 3: {
+                    num_estudiante(scanner);
+                    break;
+                }
+                case 4: {
+                    insert_asignatura(scanner);
+                    break;
+                }
+                case 5: {
+                    modificarAulaAsignatura(scanner);
+                    break;
+                }
+                case 6: {
+                    eliminarAsignatura(scanner);
+                    break;
+                }
+                case 7:{
+                    inicio=false;
+                    break;
+                }
+                default:
+                    System.out.println("error");
             }
-            case 2:{
-                mascota_estudiante(scanner);
-                break;
-            }
-            case 3:{
-                num_estudiante(scanner);
-                break;
-            }
-            case 4:{
-                insert_asignatura(scanner);
-                break;
-            }
-            case 5:{
-                break;
-            }
-            case 6:{
-                break;
-            }
-            default:
-                System.out.println("error");
         }
     }
     public static void estudiante_casa(Scanner scanner){
@@ -142,10 +152,46 @@ public class ejercicio1 {
             }
             rs.close();
             ps.close();
+            System.out.println("asignatura nueva con id : "+ idgenerado);
             System.out.println("Asignatura insertada con exito");
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.err.println("error al insertar la asignatura");
+        }
+    }
+    public static void modificarAulaAsignatura(Scanner scanner){
+        System.out.println("dime el id de la asignatura");
+        int id_asig = scanner.nextInt();
+        System.out.println("dime la nueva aula");
+        scanner.nextLine();
+        String aula_nueva =scanner.nextLine();
+        try(Connection bdd =  DriverManager.getConnection("jdbc:postgresql://ad-postgres.cljzaoejhj87.us-east-1.rds.amazonaws.com:5432/hogwarts","postgres","12345678")){
+            String modificacion= "UPDATE Asignatura SET aula=? WHERE id_asignatura=?;";
+            PreparedStatement ps= bdd.prepareStatement(modificacion);
+            ps.setString(1,aula_nueva);
+            ps.setInt(2,id_asig);
+            int fila = ps.executeUpdate();
+            System.out.println("filas afectadas :" + fila);
+            ps.close();
+        }catch (SQLException e){
+            System.out.println("error al modificar el aula " +  e.getMessage());
+        }
+
+
+
+    }
+    public static void eliminarAsignatura (Scanner scanner){
+        System.out.println("dime el id de la asignatura");
+        int id_asig= scanner.nextInt();
+        try(Connection bdd =  DriverManager.getConnection("jdbc:postgresql://ad-postgres.cljzaoejhj87.us-east-1.rds.amazonaws.com:5432/hogwarts","postgres","12345678")){
+            String eliminacion = "DELETE FROM Asignatura WHERE id_asignatura=?;";
+            PreparedStatement ps= bdd.prepareStatement(eliminacion);
+            ps.setInt(1,id_asig);
+            ps.executeUpdate();
+            ps.close();
+            System.out.println("Asigntura eliminada correctamente");
+        } catch (SQLException e ) {
+            System.out.println("error al eliminar la asignatura "+ e.getMessage());
         }
     }
 }
